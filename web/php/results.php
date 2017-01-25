21e5808a7363b2efaac0f3c44f8ee685b916c1be
Ian McClellan <im2141@bard.edu>
2017-01-25 14:53:47 -0500

    semicolons

--- web/php/results.php
+++ web/php/results.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Ian
- * Date: 1/25/17
- * Time: 1:42 PM
- */
+* Created by PhpStorm.
+* User: Ian
+* Date: 1/25/17
+* Time: 1:42 PM
+*/
 $url = parse_url(getenv("CLEARDB_DATABASE_URL")); // Heroku lets db info be hidden in environment vars
 
 $host = $url["host"];
@@ -17,41 +17,38 @@ $conn = new mysqli($host, $user, $password, $db);
 $request = $_GET["req"];
 $data = $_GET["data"];
 
-if ($request == "userData") {
-    $query = "SELECT * FROM connections WHERE id = ".$data." ";
 
-    $response = $conn->query($query);
-    for ($set = array(); $row = $response->fetch_assoc(); $set[] = $row);
+$query = "SELECT * FROM connections WHERE id = ".$data." ";
 
-    //$email = $set["email"];
-    $age = $set["age"];
-    $group = $set["group"];
-    $connections = $set["connections"];
+$response = $conn->query($query);
+for ($set = array(); $row = $response->fetch_assoc(); $set[] = $row);
 
-    $query_ages = "SELECT AVG(connections) AS 'Total', age
-                FROM connectors
-                GROUP BY age"
-    $response = $conn->query($query_ages);
-    for ($set_ages = array(); $row = $response->fetch_assoc(); $set_ages[] = $row);
+//$email = $set["email"];
+$age = $set["age"];
+$group = $set["group"];
+$connections = $set["connections"];
 
-    $query_groups = "SELECT AVG(connections) AS 'Total', group
-                FROM connectors
-                GROUP BY group"
-    $response = $conn->query($query_groups);
-    for ($set_groups = array(); $row = $response->fetch_assoc(); $set_groups[] = $row);
+$query_ages = "SELECT AVG(connections) AS 'Total', age
+            FROM connectors
+            GROUP BY age";
+$response = $conn->query($query_ages);
+for ($set_ages = array(); $row = $response->fetch_assoc(); $set_ages[] = $row);
 
-    $query_total = "SELECT AVG(connections) AS 'Total'
-                FROM connectors";
+$query_groups = "SELECT AVG(connections) AS 'Total', group
+            FROM connectors
+            GROUP BY group";
+$response = $conn->query($query_groups);
+for ($set_groups = array(); $row = $response->fetch_assoc(); $set_groups[] = $row);
 
-    $response = $conn->query($query_total);
-    for ($set_pop = array(); $row = $response->fetch_assoc(); $set_pop[] = $row);
+$query_total = "SELECT AVG(connections) AS 'Total'
+            FROM connectors";
 
-    $results = array(
-        "pop" => $set_pop,
-        "ages" => $set_ages,
-        "groups" => $set_groups,
-        "conn" => $connections
-    );
+$response = $conn->query($query_total);
+for ($set_pop = array(); $row = $response->fetch_assoc(); $set_pop[] = $row);
 
-
-}
+$results = array(
+    "pop" => $set_pop,
+    "ages" => $set_ages,
+    "groups" => $set_groups,
+    "conn" => $connections
+);
